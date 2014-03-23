2fc30c7f19a3d5de3a567bda8cfa274f00de57f4
Alok Tripathy <kullalok@gmail.com>
2014-03-22 22:05:38 -0400

    Update natlang.js

--- natlang.js
+++ natlang.js
@@ -1,27 +1,28 @@
 function replacer(a){
-	a=a.replace(/ plus /g," + ");
-	a=a.replace(/ minus /g," - ");
-	a=a.replace(/ times /g," * ");
-	a=a.replace(/ divided by /g," / ");
-	a=a.replace(/ as long as /g," while ");
-	a=a.split("\n");
+	a = a.replace(/ plus /g," + ");
+	a = a.replace(/ minus /g," - ");
+	a = a.replace(/ times /g," * ");
+	a = a.replace(/ divided by /g," / ");
+	a = a.replace(/ as long as /g," while ");
+	a = a.split("\n");
 	//aloks stuff here(a is now a list)
-	var tempa="";
+	var tempa = "";
 	for(var i in a){
-		tempa+=i+"\n";
+		tempa+=a[i]+"\n";
 	}
-	a=tempa;
+	
+	a = tempa;
 	return(a);
 }
 function change(a){
-	var b=[];
+	var b = [];
 
-	while (a.indexOf('"')>-1){
+	while (a.indexOf('"') > -1) {
 	    b.push(a.match(/\"(.*?)"/));
 	    a = a.replace(/\"(.*?)"/,"@");}
-	a=replacer(a);
+	a = replacer(a);
 	for(var i in b){
-	    a=a.replace("@",b[i][0]);
+	    a = a.replace("@",b[i][0]);
 	}
 
 	return(a);
