c1dd524b9ee4b860513ce9023ac0b05aa590e2c1
Jacob Edelman <edelman.jd@gmail.com>
2014-03-22 22:21:31 -0400

    made loops work

--- natlang.js
+++ natlang.js
@@ -1,18 +1,20 @@
 function replacer(a){
-	a = a.replace(/ plus /g," + ");
+    a = a.replace(/ plus /g," + ");
 	a = a.replace(/ minus /g," - ");
 	a = a.replace(/ times /g," * ");
 	a = a.replace(/ divided by /g," / ");
 	a = a.replace(/ as long as /g," while ");
 	a = a.split("\n");
+    console.log(a);
 	//aloks stuff here(a is now a list)
 	var tempa = "";
 	for(var i in a){
+        console.log(a[i]);
 		if(a[i].indexOf("loop")>-1){
-			var stmt=a[i].match(/loop(.*)\S/);
-			var args=stmt.replace("loop","");
+			var stmt=a[i].match(/loop(.*)\S/)[0];
+			var args=stmt.replace("loop","").replace(":","");
 			a[i]=a[i].replace(stmt,"for iiii in range("+args+"):");
-		}
+		}console.log(a[i]);
 		tempa+=a[i]+"\n";
 	}
 	
@@ -32,4 +34,3 @@ function change(a){
 
 	return(a);
 }
-	
