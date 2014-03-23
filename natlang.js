f6f4f23e19472c10529bb34c6646d2e786e13cdf
Powerroff <aaron36berger@gmail.com>
2014-03-22 22:27:38 -0400

    Update natlang.js

--- natlang.js
+++ natlang.js
@@ -20,9 +20,14 @@ function replacer(a){
 			a[i]=a[i].replace(stmt,"while not ("+args+"):");
 		}
 		if(a[i].indexOf("while")>-1){
-			var stmt=a[i].match(/until(.*)\S/)[0];
+			var stmt=a[i].match(/while(.*)\S/)[0];
 			var args=stmt.replace("while","").replace(":","");
 			a[i]=a[i].replace(stmt,"while ("+args+"):");
+		}
+		if(a[i].indexOf("if")>-1){
+			var stmt=a[i].match(/if(.*)\S/)[0];
+			var args=stmt.replace("if","").replace(":","");
+			a[i]=a[i].replace(stmt,"if ("+args+"):");
 		}
 		tempa+=a[i]+"\n";
 	}
